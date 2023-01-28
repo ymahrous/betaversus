87f14bb43df5f1af7c904f79c1768b287f739aa7
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2023-01-28 14:07:03 +0200

    commit

---
+++ sections/Insights.jsx
@@ -0,0 +1,18 @@
+'use client';
+import styles from '../styles';
+import { motion } from "framer-motion";
+import { insights } from '../constants';
+import { TitleText ,TypingText, InsightCard } from '../components';
+import { staggerContainer } from '../utils/motion';
+
+const Insights = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div variants={staggerContainer} initial='hidden' whileInView='show' viewport={{ once: false, amount: 0.25 }} className={`${styles.innerWidth} mx-auto flex flex-col`}>
+      <TypingText title='| insights' textStyles='text-center' />
+      <TitleText title='insights about betaverse' textStyles='text-center' />
+      <div className='mt-[50px] flex flex-col gap-[30px]'>{insights.map((insight, index) => (<InsightCard key={`insight-${index}`} {...insight} index={index+1} />))}</div>
+    </motion.div>
+  </section>
+);
+
+export default Insights;
---
+++ sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import About from './About';
+import World from './World';
+import Explore from './Explore';
+import Insights from './Insights';
+import WhatsNew from './WhatsNew';
+import Feedback from './Feedback';
+import GetStarted from './GetStarted';
+
+export {
+  Hero,
+  About,
+  Explore,
+  GetStarted,
+  WhatsNew,
+  World,
+  Insights,
+  Feedback,
+};
