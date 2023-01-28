420d75967e3cee55f748bc756da42f8ddb2b4352
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2023-01-28 14:19:36 +0200

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
-};
+const nextConfig = {experimental:{appDir: true}};
 
 module.exports = nextConfig;
